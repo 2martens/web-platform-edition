fa168ff6e48538cb98a5662b22f79833f657c55e
Jim Martens <jim1@live.de>
2014-12-11 15:04:13 +0100

    Fixed package name for PHP

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 # used for vagrant setup
 
 apt-get update
-apt-get install -y apache2 php mongodb
+apt-get install -y apache2 php5 mongodb
 if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant /var/www
