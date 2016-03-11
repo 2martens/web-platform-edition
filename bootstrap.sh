262f24bb5236d736345ba8cc51559af1c56a57bc
Jim Martens <github@2martens.de>
2016-03-11 13:55:43 +0100

    [Global] Updated Vagrant-related files
    * see #11
    * first in row of changes
    
    Signed-off-by: Jim Martens <github@2martens.de>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 # used for vagrant setup
 
-apt-get update
-apt-get install -y apache2 php5 mongodb
+aptitude update
+aptitude install -y apache2 php5 mongodb
 if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant /var/www
 fi
+cd /etc/apache2/sites-available
+sudo sed -i -- 's#/var/www/html#/var/www/web#g' 000-default.conf
+sudo service apache2 restart
+
+timezone="Europe/Berlin"
+sudo sed -i "s#^;date.timezone =.*#date.timezone = ${timezone}#" /etc/php5/apache2/php.ini
+sudo service apache2 restart
+
+sudo mkdir /data
+sudo chmod 777 /data
+mkdir /data/db/
+
+mongorestore /var/www/dump
